037e3b7241a3ccdf26514c29388fa8c4b3e8c5ed
repo-code <55652829+repo-code@users.noreply.github.com>
2020-04-11 17:21:22 +0300

    Update hint.js

--- src/components/hint.js
+++ src/components/hint.js
@@ -1,4 +1,3 @@
-
 import React, { useState, useCallback } from 'react'
 
 import classes from '../styles/hint.module.sass'
@@ -8,18 +7,18 @@ export const Hint = ({ expanded = false, actions = [], children }) => {
     const handleExpand = useCallback(() => setIsExpanded(!isExpanded), [isExpanded])
     return (
         <aside className={classes.root}>
-            {actions.map(({ text, onClick }, i) => (
-                <button className={classes.label} key={i} onClick={onClick}>
-                    {text}
-                </button>
-            ))}
             {isExpanded && children && <div className={classes.content}>{children}</div>}
             <menu className={classes.actions}>
-                {(
+                {children && (
                     <button className={classes.label} onClick={handleExpand}>
                         {isExpanded ? 'اخفاء التلميحات' : 'اظهار التلميحات'}
                     </button>
-                ) && children}
+                )}
+                {actions.map(({ text, onClick }, i) => (
+                    <button className={classes.label} key={i} onClick={onClick}>
+                        {text}
+                    </button>
+                ))}
             </menu>
         </aside>
     )
